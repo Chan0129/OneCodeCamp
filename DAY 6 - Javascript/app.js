c25e2e6248b42de290e733e2e80baf76db3815b6
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-03-27 19:42:40 +0800

    Added day 6

---
+++ DAY 6 - Javascript/app.js
@@ -0,0 +1,18 @@
+//Functions - Allows us to write reusable blocks of code.
+
+//Defining a function
+function greet() {
+  console.log("Hello World!");
+  console.log("Hello Philippines!");
+}
+
+//Calling a function
+greet();
+greet();
+greet();
+
+//Function with Parameter
+function greetMe(name) {
+    console.log('Hello, ', name);
+}
+greetMe('Ralph');
